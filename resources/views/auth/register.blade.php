22a394a1c0dc6a521e1cc5229aaaa19569273983
Undrakh Chingun <nikorunikk@gmail.com>
2020-02-26 02:27:40 +0800

    Register bug fixing

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,19 +11,6 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="status" class="col-md-4 col-form-label text-md-right">{{ __('status') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="status" type="text" class="form-control @error('status') is-invalid @enderror" name="status" value="{{ old('status') }}" required autocomplete="status" autofocus>
-
-                                @error('status')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
                         <div class="form-group row">
                             <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
