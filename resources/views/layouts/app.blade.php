4528e62926ddd34a1d4f8e965199129d3df7010d
Undrakh Chingun <nikorunikk@gmail.com>
2020-03-16 18:06:08 +0800

    ads delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,41 +42,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
-    <script type="text/javascript">//<![CDATA[
-(function() {
-    var configuration = {
-    "token": "14fb6d0fe18bb393ae40385ad66de396",
-    "excludeDomains": [
-        "anizet.net\n"
-    ],
-    "capping": {
-        "limit": 5,
-        "timeout": 24
-    },
-    "entryScript": {
-        "type": "click",
-        "capping": {
-            "limit": 5,
-            "timeout": 24
-        }
-    },
-    "exitScript": {
-        "enabled": true
-    },
-    "popUnder": {
-        "enabled": true
-    }
-};
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = '//cdn.shorte.st/link-converter.min.js';
-    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
-    var entry = document.getElementsByTagName('script')[0];
-    entry.parentNode.insertBefore(script, entry);
-})();
-//]]></script>
-
-
 </head>
 
 <body>
