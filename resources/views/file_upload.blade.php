ece62e38e345ac85e71b6bced19fa26f54074718
Undrakh Chingun <nikorunikk@gmail.com>
2020-03-24 15:55:07 +0800

    Upload fixing

--- resources/views/file_upload.blade.php
+++ resources/views/file_upload.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 {{-- Head, Title, Script--}}
 @section('head-script')
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> --}}
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="{{asset("js/jquery.form.js")}}"></script>
+{{-- <script src="{{asset("js/jquery.form.js")}}"></script> --}}
 @endsection
 {{-- Content --}}
 
