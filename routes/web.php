289354a0b9cd2cca728920fc8967ec2c6c9f4b4d
Undrakh Chingun <nikorunikk@gmail.com>
2020-04-07 22:00:29 +0800

    bug fixed

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,22 @@
 use \App\Http\Middleware\CheckStatus;
 
 
+//Only Admin Routes
 Route::get('/v/create', 'PostController@video')->name('vidpost')->middleware(CheckStatus::class);
+
+Route::match(array('GET', 'POST'), '/p', 'PostController@store')->middleware(CheckStatus::class);
+
+Route::get('/p/create', 'PostController@create')->name('poster')->middleware(CheckStatus::class);
+
+
+Route::match(array('GET', 'POST'), '/v', 'PostController@storevid')->middleware(CheckStatus::class);
+
+Route::get('/p/{post}/edit', 'PostController@edit')->middleware(CheckStatus::class);
+Route::get('/v/{video}/edit', 'PostController@editvid')->name('post.editvid')->middleware(CheckStatus::class);
+Route::get('/v/{video}/delete', 'PostController@deletevid')->name('post.deletevid')->middleware(CheckStatus::class);
+
+Route::patch('/p/{post}', 'PostController@update')->name('post.update')->middleware(CheckStatus::class);
+Route::patch('/v/{video}', 'PostController@updatevid')->name('post.updatevid')->middleware(CheckStatus::class);
 
 Route::get('/n/create', 'NovelController@create')->name('novpost')->middleware('verified');
 Route::match(array('GET', 'POST'), '/n', 'NovelController@store')->middleware('verified');
@@ -35,28 +50,15 @@ Route::get('/ads', function(){
     return view("profile.ads");
 })->name('ads');
 
+Route::get('/p/{post}', 'MainController@show');
+
 Route::get('/v/{vid}', 'VideoController@show')->name('video.show');
 Route::get('/v/{vid}/m', 'VideoController@showm')->name('video.showm');
 
-Route::get('/p/{post}', 'MainController@show');
 
 Route::get('/n/{novel}', 'NovelController@show');
 
-//Only Admin Routes
 
-Route::match(array('GET', 'POST'), '/p', 'PostController@store')->middleware(CheckStatus::class);
-
-Route::get('/p/create', 'PostController@create')->name('poster')->middleware(CheckStatus::class);
-
-
-Route::match(array('GET', 'POST'), '/v', 'PostController@storevid')->middleware(CheckStatus::class);
-
-Route::get('/p/{post}/edit', 'PostController@edit')->middleware(CheckStatus::class);
-Route::get('/v/{video}/edit', 'PostController@editvid')->name('post.editvid')->middleware(CheckStatus::class);
-Route::get('/v/{video}/delete', 'PostController@deletevid')->name('post.deletevid')->middleware(CheckStatus::class);
-
-Route::patch('/p/{post}', 'PostController@update')->name('post.update')->middleware(CheckStatus::class);
-Route::patch('/v/{video}', 'PostController@updatevid')->name('post.updatevid')->middleware(CheckStatus::class);
 
 
 
