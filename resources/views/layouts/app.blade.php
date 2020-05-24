588b0d55afc575c2bb5627884b5ddc70cf8e97c7
Undrakh Chingun <nikorunikk@gmail.com>
2020-05-24 21:35:49 +0800

    facebook connection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,8 +97,26 @@
             .on(500)
             .fadeOut("slow");
         });
-</script>
 
+</script>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '130959678268351',
+      xfbml      : true,
+      version    : 'v7.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
              <script src="https://kit.fontawesome.com/65a96d544f.js" crossorigin="anonymous"></script>
 
 
